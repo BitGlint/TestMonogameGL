dc81ff4d0228daaf19fdae15886f2f88d6a3fc86
BitGlint <rich@bitglint.co.uk>
2023-01-06 09:38:13 +0000

    General updates

--- TestMonogameGL/Game1.cs
+++ TestMonogameGL/Game1.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace TestMonogameGL
 {
+    /// <summary>
+    /// Main game class
+    /// </summary>
     public class Game1 : Game
     {
+        // Private variables
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _testImage;
+        private float _posX = 0;
 
+        #region Public methods
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -16,37 +28,58 @@ namespace TestMonogameGL
             IsMouseVisible = true;
         }
 
+        #endregion
+
+        #region Base class overrides
+
+        /// <summary>
+        /// Initialise game module
+        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
+        /// <summary>
+        /// Load content
+        /// </summary>
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            _testImage = Content.Load<Texture2D>("Test");
         }
 
+        /// <summary>
+        /// Main update process
+        /// </summary>
+        /// <param name="gameTime"></param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
 
-            // TODO: Add your update logic here
+            _posX += 0.1f;
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// Main draw process
+        /// </summary>
+        /// <param name="gameTime"></param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Orange);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_testImage, new Vector2(85 + (float)(Math.Sin((double)_posX) * 25), (float)(Math.Cos((double)_posX) * 15)), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        #endregion
     }
 }
